1d7a1efd4c4dc3b63e8c9fd6c162436770aaef99
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-04-06 12:13:31 +0200

    security policy seems whack

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -7,8 +7,8 @@ module.exports = [
             useDefaults: true,
             directives: {
               'connect-src': ["'self'", 'https:'],
-              'img-src': ["'self'", 'data:', 'blob:', 'yusufkakacoza.s3.eu-west-1.amazonaws.com'],
-              'media-src': ["'self'", 'data:', 'blob:', 'yusufkakacoza.s3.eu-west-1.amazonaws.com'],
+              'img-src': ["'self'", 'data:', 'blob:', 'cms.kaka.co.za', `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+              'media-src': ["'self'", 'data:', 'blob:', 'cms.kaka.co.za',`${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
               upgradeInsecureRequests: null,
             },
           },
