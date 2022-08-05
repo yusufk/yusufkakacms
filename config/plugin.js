bfde3aebdd8a9ef3dc6e19160749a19c1a626630
Yusuf Kaka <yusufkaka@microsoft.com>
2022-08-05 19:15:10 +0200

    trying port 8080

---
+++ config/plugin.js
@@ -0,0 +1,12 @@
+//  path: config/plugins.js
+
+module.exports = ({ env }) => ({
+    // ...
+    'users-permissions': {
+      config: {
+      jwtSecret: env('JWT_SECRET')
+    },
+    },
+    // ...
+  });
+  
