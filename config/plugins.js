185747d62590cd58e1b06b9b2ee2ff2aed9d1f0b
Yusuf Kaka <yusufkaka@microsoft.com>
2022-08-08 22:42:56 +0200

    Added plugins

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+//  path: config/plugins.js
+
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-storage',
+        providerOptions: {
+          account: env('STORAGE_ACCOUNT'),
+          accountKey: env('STORAGE_ACCOUNT_KEY'),
+          serviceBaseURL: env('STORAGE_URL'),
+          containerName: env('STORAGE_CONTAINER_NAME'),
+          cdnBaseURL: env('STORAGE_CDN_URL'),
+          defaultPath: 'assets',
+          maxConcurrent: 10
+        }
+      }
+    }
+  });
+  
